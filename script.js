106dba68abb1390fe02ebb360d3de48a6ee868d6
Mona Lisa <cjensen@netechnical.com>
2022-11-21 08:38:38 -0600

    added timeblocks, but they aren't fully finished

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM DD, YYYY"));
 
+var timeblock = moment()
+$("#timeblocks").text(today.format("H"));
+
+moment().get('year');
+moment().get('month'); // 0 to 11
+moment().get('date');
+moment().get('hour');
+
+
