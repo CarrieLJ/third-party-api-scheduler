73a828ff91c40dcdb1354dfc918a07cb72ae8c4a
Mona Lisa <cjensen@netechnical.com>
2022-11-23 21:59:38 -0600

    save button is now properly listening, added localStorage.setItem

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-var today = moment();
+var today = dayjs();
 $("#currentDay").text(today.format("dddd, MMMM DD, YYYY"));
-var userTasks = $('.saveBtn');
+
+
+$('.saveBtn').on('click', function(){
+//need to target the value in the block and the time
+    var textBox = $(this).siblings('.description').val();
+    var hourBox = $(this).parent().attr('id');
+//need to do a DOM traversal to move where we want within the html
+    localStorage.setItem(hourBox, textBox);
+    console.log(hourBox);  
+})
+
 
 
 //to figure out the past, present, and future of our timeblocks
 function currentTime (){
-    var currentHr = moment().hours();
+    var currentHr = dayjs().hour();
     console.log(currentHr);
 //grabbing time blocks, and .each means to loop    
     $(".time-block").each(function(){
 //define the block hour, make the string an integer, then identify the id on parent line of this (jquery style)
-        var hourBlock = parseInt($(this).attr("id").split("-")[1]);        
+        var hourBlock = parseInt($(this).attr("id").split("-")[1]);     
+        console.log($(this).attr("id"));   
         if (hourBlock < currentHr) {
             $(this).addClass("past")
         } else if (hourBlock === currentHr) {
@@ -24,22 +35,16 @@ function currentTime (){
     });
 }
 
+
+
 //in order to execute, call it outside of the function
 currentTime();
-//every 15 seconds this timer runs through the function
-var interval = setInterval(currentTime, 15000);
+// //every 15 seconds this timer runs through the function
+// var interval = setInterval(currentTime, 15000);
 
 //create function event.listenter on save button
 //click the save button
-userTasks.on('click', function () {
-    // if (isDark) {
-    //   $('body').css({ 'background-color': '#d9e9e8', color: '#1a1a1a' });
-    //   isDark = !isDark;
-    // } else {
-    //   $('body').css({ 'background-color': '#1a1a1a', color: '#d9e9e8' });
-    //   isDark = !isDark;
-    // }
-  });
+//jquery way to target the class; on is the way to create eventlistener
 // function saveDescription(event) {
 //     var btn = event.target;
     // btn.addEventListener(event) {
