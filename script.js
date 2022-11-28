1140d375574a8f8500275179b1552e3c436603bc
Mona Lisa <cjensen@netechnical.com>
2022-11-28 10:49:13 -0600

    still attempting localStorage...

--- script.js
+++ script.js
@@ -9,18 +9,24 @@ $('.saveBtn').on('click', function(){
 //need to do a DOM traversal to move where we want within the html
     localStorage.setItem(hourBox, textBox);
     // console.log(textBox);  
-
-    var hourBox = localStorage.getItem(hourBox);
-    var items = localStorage.length;
+    localStorage.getItem(hourBox);
+/////////////////////////////////////////////////
+    // document.getElementById("text").text = localStorage.getItem("textBox");
+//////////////////////////////////////////////////////
+    // var hourBox = localStorage.getItem(hourBox);
+    // var items = localStorage.length;
+// /////////////////////////////////////////
     // function getSavedText() {
-    //     var storedText = JSON.stringify(localStorage.getItem("textBox"));
-    //     if (storedText !== null) {
-    //         textBox = storedText;
-    //     }
+    //     var storedText = localStorage.getItem("hourBox");
+    //     document.getElementById("description").innerHTML = storedText;
+        // var storedText = JSON.stringify(localStorage.getItem("textBox"));
+        // if (storedText !== null) {
+        //     textBox = storedText;
+        // }
     // }
 
     // getSavedText();
-    console.log(hourBox);
+    // console.log(textBox);
     // var savedText = localStorage.length;
 });
 
@@ -29,12 +35,12 @@ $('.saveBtn').on('click', function(){
 //to figure out the past, present, and future of our timeblocks
 function currentTime (){
     var currentHr = dayjs().hour();
-    console.log(currentHr);
+    // console.log(currentHr);
 //grabbing time blocks, and .each means to loop    
     $(".time-block").each(function(){
 //define the block hour, make the string an integer, then identify the id on parent line of this (jquery style)
         var hourBlock = parseInt($(this).attr("id").split("-")[1]);     
-        console.log($(this).attr("id"));   
+        // console.log($(this).attr("id"));   
         if (hourBlock < currentHr) {
             $(this).addClass("past")
         } else if (hourBlock === currentHr) {
